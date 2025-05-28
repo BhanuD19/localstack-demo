c07de9f93b6ea54b9b86d5cd4bac7db045c58d49
BhanuD19 <vaibhav.dounde1996@hotmail.com>
2025-05-29 03:38:16 +0530

    feature/security: Updated upload function to encrypt data. Updated download function to decrypt data.

--- src/main/java/com/example/localstack/service/impl/s3ServiceImpl.java
+++ src/main/java/com/example/localstack/service/impl/s3ServiceImpl.java
@@ -1,11 +1,14 @@
 package com.example.localstack.service.impl;
 
+import com.example.localstack.config.AwsConfig;
 import com.example.localstack.service.S3Services;
 import io.awspring.cloud.s3.S3Template;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.services.kms.KmsAsyncClient;
+import software.amazon.awssdk.services.kms.model.DecryptRequest;
+import software.amazon.awssdk.services.kms.model.DecryptResponse;
 import software.amazon.awssdk.services.kms.model.EncryptRequest;
 import software.amazon.awssdk.services.kms.model.EncryptResponse;
 
@@ -20,13 +23,13 @@ import java.util.concurrent.TimeoutException;
 @Service
 public class S3ServiceImpl implements S3Services {
     private final S3Template s3Template;
-
-    private String kmsKeyId;
     private final KmsAsyncClient kmsAsyncClient;
+    private final AwsConfig awsConfig;
 
-    public S3ServiceImpl(S3Template s3Template, KmsAsyncClient kmsAsyncClient) {
+    public S3ServiceImpl(S3Template s3Template, KmsAsyncClient kmsAsyncClient, AwsConfig awsConfig) {
         this.s3Template = s3Template;
         this.kmsAsyncClient = kmsAsyncClient;
+        this.awsConfig = awsConfig;
     }
 
     /**
@@ -37,9 +40,15 @@ public class S3ServiceImpl implements S3Services {
     @Override
     public void upload(String bucketName, String key, InputStream inputStream) {
         log.info("Uploading file to S3 bucket: {}", bucketName);
-        s3Template.createBucket(bucketName);
-        s3Template.upload(bucketName, key, encryptContent(inputStream));
-        log.info("File uploaded successfully with key: {}", key);
+        try {
+            s3Template.createBucket(bucketName);
+            InputStream encryptedContent = encryptContent(inputStream);
+            s3Template.upload(bucketName, key, encryptedContent);
+            log.info("File uploaded successfully with key: {}", key);
+        } catch (Exception e) {
+            log.error("Error uploading file to S3 bucket: {}", bucketName);
+            throw new RuntimeException("Failed to upload file to S3 bucket: " + bucketName + " with key: " + key + " due to: " + e.getMessage() + "");
+        }
     }
 
     /**
@@ -50,7 +59,21 @@ public class S3ServiceImpl implements S3Services {
     @Override
     public InputStream download(String bucketName, String key) throws IOException {
         log.info("Downloading file from S3 bucket: {}", bucketName);
-        return s3Template.download(bucketName, key).getInputStream();
+        InputStream encryptedContent = s3Template.download(bucketName, key).getInputStream();
+        return decryptContent(encryptedContent);
+    }
+
+    private InputStream decryptContent(InputStream encryptedContent) {
+        try {
+            byte[] content = encryptedContent.readAllBytes();
+            DecryptRequest decryptRequest = DecryptRequest.builder().ciphertextBlob(SdkBytes.fromByteArray(content)).build();
+            DecryptResponse decryptResponse = kmsAsyncClient.decrypt(decryptRequest).get(5, TimeUnit.SECONDS);
+            log.info("Decrypted content using kmskey: {}", awsConfig.kmsKeyId());
+            return decryptResponse.plaintext().asInputStream();
+        } catch (ExecutionException | InterruptedException | TimeoutException | IOException e) {
+            log.error("Error decrypting content :", e);
+            throw new RuntimeException("failed to decrypt content", e);
+        }
     }
 
     /**
@@ -66,13 +89,16 @@ public class S3ServiceImpl implements S3Services {
     }
 
     private InputStream encryptContent(InputStream content) {
-        EncryptRequest encryptRequest = EncryptRequest.builder().keyId(kmsKeyId).plaintext(SdkBytes.fromInputStream(content)).build();
-        EncryptResponse encryptResponse;
         try {
+            byte[] contentBytes = content.readAllBytes();
+            EncryptRequest encryptRequest = EncryptRequest.builder().keyId(awsConfig.kmsKeyId()).plaintext(SdkBytes.fromByteArray(contentBytes)).build();
+        EncryptResponse encryptResponse;
+
             encryptResponse = kmsAsyncClient.encrypt(encryptRequest).get(5, TimeUnit.SECONDS);
-        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+        log.info("Encrypted content using kmskey: {}", awsConfig.kmsKeyId());
+        return encryptResponse.ciphertextBlob().asInputStream();
+        } catch (InterruptedException | ExecutionException | TimeoutException | IOException e) {
             throw new RuntimeException(e);
         }
-        return encryptResponse.ciphertextBlob().asInputStream();
     }
 }
